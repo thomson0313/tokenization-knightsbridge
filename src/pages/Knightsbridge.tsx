49a5fd974167d9758f20cdece09945b8f06ed9fe
thomson0313 <thomson19960313@gmail.com>
2025-06-11 08:29:30 -0700

    feat: updated payment panel sidebar

--- src/pages/Knightsbridge.tsx
+++ src/pages/Knightsbridge.tsx
@@ -64,7 +64,7 @@ const Knightsbridge: React.FC<KnightsbridgeProps> = ({ isDarkMode, onThemeToggle
           </div>
         </section>
 
-        <div className="flex flex-col xl:flex-row gap-4 xl:gap-7 mb-[100px] px-4 md:px-8 xl:px-16 xl:absolute right-0 w-full">
+        <div className="flex flex-col xl:flex-row gap-4 xl:gap-7 mb-[100px] px-4 pb-4 md:px-8 xl:px-16 xl:absolute right-0 w-full">
           <form className="flex-[7] border bg-bg-secondary p-4 md:p-7 rounded-3xl border-border-primary">
             <KYCInformationSection />
 
